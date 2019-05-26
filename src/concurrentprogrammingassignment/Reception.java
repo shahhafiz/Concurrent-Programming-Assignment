f6fb385651d50505059efcaa3abedf74f2b17943
shahhafiz <iamshahhafiz@gmail.com>
2019-05-26 17:40:16 +0800

    Update Reception.java
    Add lines to create report in closeReception()

--- src/concurrentprogrammingassignment/Reception.java
+++ src/concurrentprogrammingassignment/Reception.java
@@ -80,7 +80,7 @@ public class Reception {
             d.addPatient(patient);
             System.out.println(patient.name+" from cwl, assigned to "+d.name);
         } else {
-            System.out.println("Common waiting list is empty");
+//            System.out.println("Common waiting list is empty");
         }
     }
     
@@ -105,10 +105,72 @@ public class Reception {
         } catch (InterruptedException ex) {} 
     }
     
-    public void closeReception(){
+   public void closeReception(){
         System.out.println("Close reception");
         stopWorking = true;
         ListNotEmptySignall();
+        
+        System.out.println("-------------------------------------------------------------------------Hospital Report---------------------------------------------------------------------------");
+
+        int totalNumberOfPatient = 0;
+        for(Doctor d: doctors){
+            totalNumberOfPatient += d.apl.getSize();
+        }
+        
+        System.out.println("Number of doctor today : "+doctors.length);
+        System.out.println("The number of patient visited : "+totalNumberOfPatient);
+        System.out.println("");
+        int overallWaitingTime=0;
+        int overallConsultaionTime=0;
+        for(Doctor doctor : doctors){
+            int totalConsultationTime=0;
+            int totalWaitingTime=0;
+            int waitingTime;
+            Patient previousPatient=new Patient("dummy",0,0);
+            int previousPatientFinishTime;
+            int currentPatientFinishTime;
+            int previousPatientWaitingTime=0;
+            int countPatient=1;
+            int doctorBreak;
+            for(Patient patient : doctor.apl.getPatients())
+            {
+                waitingTime=0;
+                
+                if(countPatient==9){
+                    doctorBreak=15;
+                }
+                else{
+                    doctorBreak=0;
+                }
+                previousPatientFinishTime=previousPatient.arrivalTime+previousPatientWaitingTime+previousPatient.consultationTime+doctorBreak;
+                if(previousPatientFinishTime<=patient.arrivalTime){
+                    waitingTime=0+doctorBreak;
+                } else {
+                    waitingTime=previousPatient.arrivalTime+previousPatient.consultationTime+previousPatientWaitingTime-patient.arrivalTime+doctorBreak;
+                }
+                
+                totalConsultationTime+=patient.consultationTime;
+                currentPatientFinishTime = patient.arrivalTime + waitingTime + patient.consultationTime+doctorBreak; 
+//                System.out.println("count : "+countPatient+" break : "+doctorBreak);
+//                System.out.println(patient.name+ " waiting time: "+waitingTime+" "+patient.arrivalTime+" "+patient.consultationTime + "("+currentPatientFinishTime+")");
+//                System.out.println("total consultaton time "+totalConsultationTime);
+                totalWaitingTime += waitingTime;
+                previousPatient=patient;
+                previousPatientWaitingTime = waitingTime;
+                countPatient++;
+            }
+            overallWaitingTime+=totalWaitingTime;
+            overallConsultaionTime+=totalConsultationTime;
+            System.out.println("Doctor name : "+doctor.name);
+            System.out.println("Total number of patient seen by doctor : "+doctor.apl.getSize());
+            System.out.println("Total waiting time : "+totalWaitingTime);
+            System.out.println("Average patient waiting time :"+totalWaitingTime/doctor.apl.getSize());
+            System.out.println("Total consultation time : "+totalConsultationTime);
+            System.out.println("Average consultaion time: "+totalConsultationTime/doctor.apl.getSize()+"\n");
+
+        }
+        System.out.println("Overall waiting time: "+overallWaitingTime);
+        System.out.println("Overall consultation time: "+overallConsultaionTime);
     }
 }
 
